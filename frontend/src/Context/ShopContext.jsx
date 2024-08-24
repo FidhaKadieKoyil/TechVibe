f064b45cca859806a813cd0157b1a9f8a957cc8c
Fidha K K <fidhasafeer6@gmail.com>
2024-08-24 12:33:38 -0400

    changed

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -15,7 +15,7 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
     useEffect(() => {
-        fetch('https://fidhakadiekoyil.github.io/')
+        fetch('https://fidhakadiekoyil.github.io/TechVibe/')
             .then((response) => response.json())
             .then((data) => setAll_Product(data))
             .catch((error) => console.error('Error fetching products:', error));
